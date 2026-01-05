6b40e97143d54c5690c96928531de7f886b1f652
t0nyware <tugrulnazarov@gmail.com>
2026-01-05 17:37:18 +0000

    affirmative

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
